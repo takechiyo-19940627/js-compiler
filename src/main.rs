a6aa977fe10eea4d10cfe047c90e709189d482b8
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2025-07-24 22:41:37 +0900

    feat: トークンの定義

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Token {
+    /// Token Type
+    pub kind: Kind,
+
+    /// Start offset in source
+    pub start: usize,
+
+    /// End offset in source
+    pub end: usize,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Kind {
+    Eof, // end of file
+    Plus,
+}
+
 fn main() {
     println!("Hello, world!");
 }
