eb880d0e298cf6257de2f013b2e7f6e8099eb789
takehiro <45512992+takechiyo-19940627@users.noreply.github.com>
2025-07-24 22:58:20 +0900

    feat: 字句解析の追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::str::Chars;
+
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Token {
     /// Token Type
@@ -14,6 +16,42 @@ pub struct Token {
 pub enum Kind {
     Eof, // end of file
     Plus,
+}
+
+struct Lexer<'a> {
+    source: &'a str,
+
+    chars: Chars<'a>,
+}
+
+impl<'a> Lexer<'a>  {
+    pub fn new(source: &'a str) -> Self {
+        Self{
+            source,
+            chars: source.chars(),
+        }
+    }
+
+    fn read_next_kind(&mut self) -> Kind {
+        while let Some(c) = self.chars.next() {
+            match c {
+                '+' => return Kind::Plus,
+                _ => {}
+            }
+        }
+        Kind::Eof
+    }
+
+    fn read_next_token(&mut self) -> Token {
+        let start = self.offset();
+        let kind = self.read_next_kind();
+        let end = self.offset();
+        Token{ kind, start, end }
+    }
+
+    fn offset(&self) -> usize {
+        self.source.len() - self.chars.as_str().len()
+    }
 }
 
 fn main() {
